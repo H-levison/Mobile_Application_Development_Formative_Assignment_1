7f3f0c8ba53c7a5813b7863313d2974370ff1cca
H-levison <h.levison@alustudent.com>
2026-02-10 07:43:13 +0200

    Added the color theme for the project

---
+++ alu_academic_assistant/lib/theme/alu_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ALUColors {
+  static const Color primaryMaroon = Color(0xFF73000a);
+  static const Color primaryGold = Color(0xFFb59a57);
+  static const Color background = Color(0xFFF7F7F7);
+  static const Color textBlack = Color(0xFF1A1A1A);
+  static const Color warningRed = Color(0xFFD32F2F);
+  static const Color successGreen = Color(0xFF388E3C);
+}
